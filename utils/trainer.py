8b56d533731c2e5aac2294b979ca753541461bf4
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-14 18:40:41 +0100

    debug

--- utils/trainer.py
+++ utils/trainer.py
@@ -59,7 +59,7 @@ def train_and_evaluate(C, lr, lambda_reg, alpha, subgradient_step, w0, r,
         for i, data in enumerate(trainloader, 0):
             
             if(i % 10 == 0):
-                clear_output(wait=True)
+                #clear_output(wait=True)
                 print(i) 
             print("aaa")
             inputs, labels = data[0].to(device), data[1].to(device)
