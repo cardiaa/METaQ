a1b9a20926a517c2f7376efb20fff5f9f8247bbc
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-20 21:19:53 +0100

    Update train.py

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ def train_model(args):
 
     set_affinity(process_index, num_processes)  # Commentata per ora
 
-    torch.set_num_threads(1)
+    #torch.set_num_threads(1)
     trainset, testset = load_data()  # Carichiamo i dati localmente
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=0)
     testloader = torch.utils.data.DataLoader(testset, batch_size=1000, shuffle=False, num_workers=0)
