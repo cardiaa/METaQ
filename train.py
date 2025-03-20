0969deab93d35699bc9beb0a7a9945d88af038b6
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-20 19:58:54 +0100

    Update train.py

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ def train_model(args):
 
 
 if __name__ == "__main__":
-    num_processes = 24
+    num_processes = 12
     num_total_cores = os.cpu_count()  
 
     print(f"Numero di processi: {num_processes}")
@@ -72,7 +72,7 @@ if __name__ == "__main__":
         "alpha": [0.533],
         "subgradient_step": [1e5],
         "w0": [-0.11],
-        "r": [round(1.1 + i * 0.002, 3) for i in range(24)],
+        "r": [round(1.1 + i * 0.002, 3) for i in range(12)],
         "target_acc": [98.99],
         "target_entr": [0.99602e6],
         "min_xi": [0],
