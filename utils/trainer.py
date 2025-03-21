394064572f629e62710faae1147f9ddb8226bf46
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-21 13:50:05 +0100

    Update trainer.py

--- utils/trainer.py
+++ utils/trainer.py
@@ -57,7 +57,7 @@ def train_and_evaluate(C, lr, lambda_reg, alpha, subgradient_step, w0, r,
         start_time = time.time()
         for i, data in enumerate(trainloader, 0):
             if(i % 10 == 0):
-                print(i)
+                print(i, device)
             inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
