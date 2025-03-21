1106013aacd57abc55fa8a0dd38165ebedb9259b
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-21 13:28:21 +0100

    Update train.py

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
 
     multiprocessing.set_start_method('spawn', force=True)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    #device = torch.device("cpu")
+    print(device)
     np.set_printoptions(precision=6)
 
     param_grid = {
