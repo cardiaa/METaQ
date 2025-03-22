9ebf3eee7ee5d4ff617ba98d8c5567b6cec93696
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-22 14:18:32 +0100

    updates

--- train.py
+++ train.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     np.set_printoptions(precision=6)
 
     param_grid = {
-        "C": [2],
+        "C": [6],
         "lr": [0.0007],
         "lambda_reg": [0.0015],
         "alpha": [0.533],
