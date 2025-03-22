a550cccf406d0ba05b12d195987d70e5a3486c84
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-22 22:27:18 +0100

    Update train.py

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ def train_model(args):
     set_affinity(process_index, num_processes)  # Commentata per ora
 
     torch.set_num_threads(1)
-    time.sleep(0.5)
+    
     
     #print(f"Process {process_index}: torch.get_num_threads() = {torch.get_num_threads()}")
     #print(f"Process {process_index}: Affinity = {os.sched_getaffinity(0)}", flush=True)
@@ -40,6 +40,8 @@ def train_model(args):
     trainset, testset = load_data()  # Carichiamo i dati localmente
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=0)
     testloader = torch.utils.data.DataLoader(testset, batch_size=1000, shuffle=False, num_workers=0)
+
+    time.sleep(3)
 
     (C, lr, lambda_reg, alpha, subgradient_step, w0, r,
      target_acc, target_entr, min_xi, max_xi, n_epochs,
