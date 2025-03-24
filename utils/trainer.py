4e4c5b5d3f75dc6ffb86b244f4a3a14239d8deda
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-24 15:21:39 +0100

    test

--- utils/trainer.py
+++ utils/trainer.py
@@ -43,7 +43,6 @@ def train_and_evaluate(C, lr, lambda_reg, alpha, subgradient_step, w0, r,
     
     torch.set_num_threads(1)
     print("Inizio caricamento...")
-    time.sleep(5)
     trainset, testset = load_data()  # Carichiamo i dati localmente
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=0)
     testloader = torch.utils.data.DataLoader(testset, batch_size=1000, shuffle=False, num_workers=0)
