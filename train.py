64c275cb50dd39444770670074f25c56c4d0253f
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-24 16:17:20 +0100

    test

--- train.py
+++ train.py
@@ -59,7 +59,7 @@ def worker(semaphore, args):
     return train_model(args)
 
 
-def run_in_parallel(param_combinations, num_processes, max_wait_time=1.9):
+def run_in_parallel(param_combinations, num_processes, max_wait_time=2):
     semaphore = multiprocessing.Semaphore(0)  # Semaforo inizializzato a 0
     processes = []
     results = []
