ed06ec7dff7ba5b902e703e8ac4b349b2d0b493f
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-24 17:54:04 +0100

    test

--- utils/trainer.py
+++ utils/trainer.py
@@ -88,6 +88,7 @@ def train_and_evaluate(C, lr, lambda_reg, alpha, subgradient_step, w0, r,
             optimizer.step()
 
             if i == 30 and epoch == 0:
+                print("XXXXXXX")
                 semaphore.release()  # Rilascia il semaforo quando arrivo fin qua
                 # Registra il tempo di rilascio del semaforo
                 release_times[process_id] = time.time()
