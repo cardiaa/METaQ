e0f6442defd83aef9f0b07f68bc996f885f41c75
Andrea Cardia <andreacardia2008@gmail.com>
2025-03-24 18:01:24 +0100

    test

--- train.py
+++ train.py
@@ -76,7 +76,7 @@ def run_in_parallel(param_combinations, num_processes, max_wait_time=2.5):
 
 
 if __name__ == "__main__":
-    num_processes = 12  # Numero desiderato di processi
+    num_processes = 24  # Numero desiderato di processi
     num_total_cores = os.cpu_count()  
 
     print(f"Numero di processi: {num_processes}")
