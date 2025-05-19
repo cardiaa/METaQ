2e87a1389bdca14e924d75195c1911f89379d481
Andrea Cardia <andreacardia2008@gmail.com>
2025-05-19 13:19:55 +0200

    Pruning Test: delta=31

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     device = torch.device("cpu")  
     train_optimizer = "A"  
     entropy_optimizer = "F"  
-    delta = 3
+    delta = 31
     pruning = "Y"
     
     train_and_evaluate(
