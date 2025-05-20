5e8d5c87ecca6b92e4ef5f2cfc33cc9fc96f7de6
Andrea Cardia <andreacardia2008@gmail.com>
2025-05-20 08:35:42 +0200

    No-pruning Test: target_entr_before = 2e4

--- utils/trainer.py
+++ utils/trainer.py
@@ -123,7 +123,7 @@ def train_and_evaluate(C, lr, lambda_reg, alpha, subgradient_step, w0, r,
         w_quantized_before = quantize_weights_center(w, v, v_centers_before)
         encoded_list_before = [float(elem) if float(elem) != -0.0 else 0.0 for elem in w_quantized_before]
         quantized_entropy_before = round(compute_entropy(encoded_list_before)) + 1
-        target_entr_before = 1e4
+        target_entr_before = 2e4
         
         # Saving a better model
         # if(entropies[-1] <= target_entr):
