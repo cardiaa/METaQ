89b7b522d3a2ca2120596f1540bde72302f672ec
Andrea Cardia <andreacardia2008@gmail.com>
2025-06-22 07:20:53 +0200

    TEST [Dantzig]: C=64, delta=8

--- utils/trainer.py
+++ utils/trainer.py
@@ -9,10 +9,10 @@ from utils.optimization import FISTA, ProximalBM, test_accuracy
 from utils.weight_utils import initialize_weights
 from utils.quantize_and_compress import compress_zstd, BestQuantization, pack_bitmask
 
-def train_and_evaluate(model, criterion, C, lr, lambda_reg, alpha, subgradient_step, w0, r,
+def train_and_evaluate(model, criterion, C, lr, lambda_reg, alpha, subgradient_step, w0, r, first_best_indices,
                         BestQuantization_target_acc, final_target_acc, target_zstd_ratio, min_xi, max_xi, upper_c, lower_c, 
                         c1, c2, zeta, l, n_epochs, max_iterations, device, train_optimizer, entropy_optimizer, 
-                        trainloader, testloader, delta, pruning, QuantizationType, sparsity_threshold):
+                        trainloader, testloader, delta, pruning, QuantizationType, sparsity_threshold, accuracy_tollerance):
     
     torch.set_num_threads(1)
     
@@ -161,9 +161,10 @@ def train_and_evaluate(model, criterion, C, lr, lambda_reg, alpha, subgradient_s
         # Saving a better model
         if(accuracies[-1] >= BestQuantization_target_acc):
             log = BestQuantization(log=log, C=C, r=r, epoch=epoch, min_w=min_w, max_w=max_w, w=w, c1=c1, c2=c2,
-                                   final_target_acc=final_target_acc, target_zstd_ratio=target_zstd_ratio, QuantizationType=QuantizationType,
-                                   model=model, testloader=testloader, accuracy=accuracy, device=device)
-            target_acc = accuracies[-1] 
+                                   final_target_acc=final_target_acc, target_zstd_ratio=target_zstd_ratio, 
+                                   QuantizationType=QuantizationType, model=model, testloader=testloader, accuracy=accuracy, 
+                                   device=device, first_best_indices=first_best_indices, accuracy_tollerance=accuracy_tollerance)
+            BestQuantization_target_acc = accuracies[-1] 
         
         # ---------------------------------------------------------------------------------------------------------
         # No-pruning exit conditions
