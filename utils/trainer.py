994c84be53a6e9d851244d96b4a10336c45fa93a
Andrea Cardia <andreacardia2008@gmail.com>
2025-06-24 10:14:01 +0200

    TEST [Dantzig]: delta ranging in [10.400, 10.555, 0.005], TEST [Fenchel]: delta ranging in [10.560, 10.715, 0.005]

--- utils/trainer.py
+++ utils/trainer.py
@@ -162,9 +162,10 @@ def train_and_evaluate(model, criterion, C, lr, lambda_reg, alpha, subgradient_s
         # Saving a better model
         if(accuracies[-1] >= BestQuantization_target_acc):
             log = BestQuantization(log=log, C=C, r=r, delta=delta, epoch=epoch, min_w=min_w, max_w=max_w, w=w, c1=c1, c2=c2,
-                                   final_target_acc=final_target_acc, target_zstd_ratio=target_zstd_ratio, 
-                                   QuantizationType=QuantizationType, model=model, testloader=testloader, accuracy=accuracy, 
-                                   device=device, first_best_indices=first_best_indices, accuracy_tollerance=accuracy_tollerance)
+                                   final_target_acc=final_target_acc, target_zstd_ratio=target_zstd_ratio,
+                                   sparsity_threshold=sparsity_threshold, QuantizationType=QuantizationType, model=model, 
+                                   testloader=testloader, accuracy=accuracy, device=device, first_best_indices=first_best_indices, 
+                                   accuracy_tollerance=accuracy_tollerance)
             BestQuantization_target_acc = accuracies[-1] 
         
         # ---------------------------------------------------------------------------------------------------------
