0847101767890870b01f2fba07078c9d65fb0f13
Andrea Cardia <andreacardia2008@gmail.com>
2025-06-27 19:13:46 +0200

    Same test as last time but with C=128 and n_epochs=50 - Enhanced LeNet-5 - TEST [Dantzig]: delta ranging in [10.400, 10.555, 0.005], TEST [Fenchel] delta ranging in [10.560, 10.715, 0.005]

--- train.py
+++ train.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     # Define fixed hyperparameters for the model and training process
     model, model_name = LeNet5().to(device), "LeNet-5"
     criterion, criterion_name = CrossEntropyLoss(), "CrossEntropy" 
-    C = 64
+    C = 128
     lr = 0.0007  
     lambda_reg = 0.0015
     alpha = 0.533
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     accuracy_tollerance = 0.2
     zeta = 50000
     l = 0.5
-    n_epochs = 100 # To be increased as soon as I find good configurations
+    n_epochs = 50 # To be increased as soon as I find good configurations
     max_iterations = 15
     train_optimizer = "ADAM"  
     entropy_optimizer = "FISTA"  
