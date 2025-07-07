4ad45ee8d8bb40e1aa8f0625fca12c607faeba8b
Andrea Cardia <andreacardia2008@gmail.com>
2025-07-07 13:28:36 +0200

    Testing gpu0: LeNet-5_Original, lambda_reg=0.0015, alpha=0.533

--- train_on_gpus.py
+++ train_on_gpus.py
@@ -66,8 +66,8 @@ if __name__ == "__main__":
     criterion, criterion_name = CrossEntropyLoss(), "CrossEntropy" 
     C = 64
     lr = 0.001
-    lambda_reg = 0
-    alpha = 1
+    lambda_reg = 0.0015
+    alpha = 0.533
     subgradient_step = 1e5 
     bucket_zero = round((C-1)/2) #it must range from 0 to C-2
     #r = 1.1    
