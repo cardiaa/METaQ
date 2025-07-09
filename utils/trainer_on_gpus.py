dff3b2f281106f9e93196e44d29a036e00129ddf
Andrea Cardia <andreacardia2008@gmail.com>
2025-07-09 08:59:25 +0200

    Test [Dantzig]: lambda_reg = 0.0002, alpha = 0.5, delta in [6, 9.1, 0.1]

--- utils/trainer_on_gpus.py
+++ utils/trainer_on_gpus.py
@@ -40,8 +40,8 @@ def train_and_evaluate(model, criterion, C, lr, lambda_reg, alpha, subgradient_s
     for epoch in range(n_epochs):
         start_time = time.time()
         for i, data in enumerate(trainloader, 0):
-            if i % 100 == 0:
-                print(f"Batch {i} of epoch {epoch + 1}", flush=True)
+            #if i % 100 == 0:
+            #    print(f"Batch {i} of epoch {epoch + 1}", flush=True)
             inputs, targets = data
             inputs, targets = inputs.to(device), targets.to(device)
             optimizer.zero_grad()
