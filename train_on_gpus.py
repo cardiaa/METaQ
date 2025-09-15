ce466f21e065b81b48eccdad8993031557cd782c
Andrea Cardia <andreacardia2008@gmail.com>
2025-09-15 17:38:04 +0200

    Debug: test on vgg-16, alpha=1 works. Now back to alexnet alpha=1, does not work. Now try btach_size from 2048 to 512

--- train_on_gpus.py
+++ train_on_gpus.py
@@ -78,8 +78,8 @@ def load_data(model_name):
 
         train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=local_rank, shuffle=True, drop_last=True)
 
-        trainset = DataLoader(train_dataset, batch_size=2048, sampler=train_sampler, num_workers=8, pin_memory=True)
-        testset = DataLoader(val_dataset, batch_size=2048, shuffle=False, num_workers=8, pin_memory=True)
+        trainset = DataLoader(train_dataset, batch_size=512, sampler=train_sampler, num_workers=8, pin_memory=True)
+        testset = DataLoader(val_dataset, batch_size=512, shuffle=False, num_workers=8, pin_memory=True)
     elif(model_name == "VGG16"):
         transform_train = transforms.Compose([
             transforms.RandomResizedCrop(224),
@@ -137,7 +137,7 @@ if __name__ == "__main__":
 
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
-        if(model_name != "AlexNet" or model_name != "VGG16"):
+        if(model_name == "AlexNet" or model_name == "VGG16"):
             print(f"Using device {device} ({torch.cuda.get_device_name(device)})", flush=True)
     else:
         device = torch.device("cpu")
