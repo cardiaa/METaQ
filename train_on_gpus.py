7bdd50aa38899fc2fa1f7f2caf7578b09278ba9e
Andrea Cardia <andreacardia2008@gmail.com>
2025-09-15 18:28:32 +0200

    Debug: test on vgg-16, alpha=1 works. Now back to alexnet alpha=1, does not work. Now try btach_size from 2048 to 512, lr=1e-4

--- train_on_gpus.py
+++ train_on_gpus.py
@@ -216,7 +216,7 @@ if __name__ == "__main__":
         model = DDP(model, device_ids=[local_rank])     
         criterion, criterion_name = nn.CrossEntropyLoss(), "CrossEntropy" 
         C = 32
-        lr = 0.01
+        lr = 1e-4
         lambda_reg = 0.01
         alpha = 1
         subgradient_step = 1e5 
