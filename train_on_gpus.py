0fce487b806d1d55390ee1c976bc791e566906e8
Andrea Cardia <andreacardia2008@gmail.com>
2025-09-16 08:52:53 +0200

    Back to alpha=1: batch_size from 128 to 256, lr from 1e-3 to 2e-3

--- train_on_gpus.py
+++ train_on_gpus.py
@@ -49,6 +49,7 @@ def load_data(model_name):
             trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
             # Load the test set of MNIST dataset with the specified transformation
             testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)   
+    # --------------------------------------------------------------------------------------------------------------
     elif(model_name[:12] == "LeNet300_100"):
         transform = transforms.Compose([
             transforms.ToTensor(),
@@ -56,6 +57,7 @@ def load_data(model_name):
         ])
         trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
         testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    # --------------------------------------------------------------------------------------------------------------
     elif(model_name == "AlexNet"):
         transform_train = transforms.Compose([
             transforms.RandomResizedCrop(224),
@@ -78,8 +80,9 @@ def load_data(model_name):
 
         train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=local_rank, shuffle=True, drop_last=True)
 
-        trainset = DataLoader(train_dataset, batch_size=128, sampler=train_sampler, num_workers=8, pin_memory=True)
-        testset = DataLoader(val_dataset, batch_size=128, shuffle=False, num_workers=8, pin_memory=True)
+        trainset = DataLoader(train_dataset, batch_size=256, sampler=train_sampler, num_workers=8, pin_memory=True)
+        testset = DataLoader(val_dataset, batch_size=256, shuffle=False, num_workers=8, pin_memory=True)
+    # --------------------------------------------------------------------------------------------------------------
     elif(model_name == "VGG16"):
         transform_train = transforms.Compose([
             transforms.RandomResizedCrop(224),
@@ -216,7 +219,7 @@ if __name__ == "__main__":
         model = DDP(model, device_ids=[local_rank])     
         criterion, criterion_name = nn.CrossEntropyLoss(), "CrossEntropy" 
         C = 32
-        lr = 1e-3
+        lr = 2e-3
         lambda_reg = 5e-4
         alpha = 1
         subgradient_step = 1e5 
