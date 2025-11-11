7075ba4bb4232096c1729632f2b8a3cb8313f47e
Andrea Cardia <andreacardia2008@gmail.com>
2025-11-11 17:37:41 +0100

    Debug: alpha=1

--- train_on_gpus.py
+++ train_on_gpus.py
@@ -222,7 +222,7 @@ if __name__ == "__main__":
         lr = 1.6e-2
         batch_size = 2048
         lambda_reg = 5e-4
-        alpha = 0.9
+        alpha = 1
         subgradient_step = 1e5 
         bucket_zero = round((C-1)/2) #it must range from 0 to C-2
         r = 2
