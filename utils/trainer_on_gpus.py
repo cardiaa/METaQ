0c01e06f0c4ebf5fe9dc2c20bc0c20de874c57df
Andrea Cardia <andreacardia2008@gmail.com>
2025-11-19 13:48:09 +0100

    Deleted bucket 0; Back to r and w0 ad free parameters; ...Testing plausible values...

--- utils/trainer_on_gpus.py
+++ utils/trainer_on_gpus.py
@@ -63,6 +63,10 @@ def train_and_evaluate(model, model_name, criterion, C, lr, lambda_reg, alpha, s
             #    print(f"Batch {i} of epoch {epoch + 1}: time {round(time.time() - start_time2, 2)}s", flush=True)
             if((model_name == "AlexNet" or model_name == "VGG16") and local_rank == 0):
                 print(f"Batch {i} of epoch {epoch + 1}: time {round(time.time() - start_time2, 2)}s", flush=True)
+                w = torch.cat([param.detach().view(-1) for param in model.parameters()]).to(device)
+                qs = torch.tensor([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+                valori = torch.quantile(w, qs)
+                print(f"  Quantiles of weights: {valori.tolist()}", flush=True)
             start_time2 = time.time()
             inputs, targets = data
             inputs, targets = inputs.to(device), targets.to(device)
