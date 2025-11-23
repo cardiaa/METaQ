7ae49d4fa7e10345f08654cd2577ae2dd1a3547f
Andrea Cardia <andreacardia2008@gmail.com>
2025-11-23 13:59:31 +0100

    Test with T1=5e-4, T2=5e-8

--- train_on_gpus.py
+++ train_on_gpus.py
@@ -223,7 +223,7 @@ if __name__ == "__main__":
         batch_size = 2048
         lambda_reg = 5e-4
         alpha = 0.99999
-        T1_explicit = 5e-3
+        T1_explicit = 5e-4
         T2_explicit = 5e-8
         subgradient_step = 1e5 
         bucket_zero = round((C-1)/2) #it must range from 0 to C-2
