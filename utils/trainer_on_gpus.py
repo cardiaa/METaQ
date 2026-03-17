bb03d422edda646440cfa890e1ff6293b76f0db0
Andrea Cardia <andreacardia2008@gmail.com>
2026-03-17 11:41:49 +0100

    Adjusted printings

--- utils/trainer_on_gpus.py
+++ utils/trainer_on_gpus.py
@@ -78,26 +78,28 @@ def train_and_evaluate(model, model_name, criterion, C, lr, lambda_reg, alpha, T
         #if local_rank == 0:
         #    print(f"Beginning epoch {epoch} at {(datetime.now() + timedelta(hours=2)).strftime('%Y-%m-%d %H:%M:%S')}", flush=True)
         start_time_global = time.time()
-        start_time2 = time.time()
+        
         for i, data in enumerate(trainloader, 0):
             #if i % 100 == 0:
             #if((model_name[:7] == "LeNet-5" or model_name == "LeNet300_100") and delta == 5): 
             #    print(f"Batch {i} of epoch {epoch + 1}: time {round(time.time() - start_time2, 2)}s", flush=True)
             if((model_name == "AlexNet" or model_name == "VGG16") and local_rank == 0):
-                if i % 1 == 0:
-                    print(f"Batch {i} of epoch {epoch + 1}: time {round(time.time() - start_time2, 2)}s", flush=True)
+                if i == 0:
                     start_time2 = time.time()
-                    """
-                    w = torch.cat([param.detach().view(-1) for param in model.parameters()]).to(device)
-                    num_samples = 1000000
-                    idx = torch.randperm(w.numel(), device=w.device)[:num_samples]
-                    w_sample = w[idx]                
-                    qs = torch.tensor([0.0, 0.25, 0.5, 0.75, 1.0])
-                    qs = qs.to(w.device)
-                    valori = torch.quantile(w_sample, qs)
-                    valori_rounded = [round(v.item(), 4) for v in valori]
-                    print(f"Quartiles of weights: {valori_rounded}", flush=True)
-                    """
+                else:
+                    print(f"Ended batch {i} of epoch {epoch + 1}: time {round(time.time() - start_time2, 2)}s", flush=True)
+                    start_time2 = time.time()
+                """
+                w = torch.cat([param.detach().view(-1) for param in model.parameters()]).to(device)
+                num_samples = 1000000
+                idx = torch.randperm(w.numel(), device=w.device)[:num_samples]
+                w_sample = w[idx]                
+                qs = torch.tensor([0.0, 0.25, 0.5, 0.75, 1.0])
+                qs = qs.to(w.device)
+                valori = torch.quantile(w_sample, qs)
+                valori_rounded = [round(v.item(), 4) for v in valori]
+                print(f"Quartiles of weights: {valori_rounded}", flush=True)
+                """
                     
             inputs, targets = data
             inputs, targets = inputs.to(device), targets.to(device)
